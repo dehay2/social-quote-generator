8179d0c809bbe96ee18eb562e3a38bceffabb3d5
dehay2 <dejapatterson2698@gmail.com>
2025-12-22 11:46:06 -0500

    Updated JS/HTML and added AI API

--- src/script.js
+++ src/script.js
@@ -1,11 +1,25 @@
-function generateQuote(event) {
-  event.preventDefault();
-
+function displayQuote(response) {
   new Typewriter("#quote", {
-    strings: "“After all, one can’t complain. I have my friends.” —Eeyore.",
+    strings: response.data.answer,
     autoStart: true,
     delay: 40,
   });
+}
+
+function generateQuote(event) {
+  event.preventDefault();
+
+  let topicInput = document.querySelector("#user-instructions");
+  let apiKey = "07c173115a88142ao0a457t94aeaf9b8";
+  let context =
+    "You are wise and knowledgable quote generator. Provide a quote within one to two sentences in basic HTML format. Make sure to follow the user instructions.";
+  let prompt = `User instructions: generate a quote about ${topicInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiURL).then(displayQuote);
 }
 
 let quoteGeneratorElement = document.querySelector("#social-quote-generator");
