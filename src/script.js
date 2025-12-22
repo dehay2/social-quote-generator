dfa8368d1134d7ec62b9fdabd152c079afcd481a
dehay2 <dejapatterson2698@gmail.com>
2025-12-22 00:37:27 -0500

    added JS and typewriter plugin

--- src/script.js
+++ src/script.js
@@ -1,1 +1,12 @@
+function generateQuote(event) {
+  event.preventDefault();
 
+  new Typewriter("#quote", {
+    strings: "“After all, one can’t complain. I have my friends.” —Eeyore.",
+    autoStart: true,
+    delay: 40,
+  });
+}
+
+let quoteGeneratorElement = document.querySelector("#social-quote-generator");
+quoteGeneratorElement.addEventListener("submit", generateQuote);
